e99db86c14dae54dceeec03bd6a66c13d1bd9faf
Ashikur Rahman Rashid <ashikurrahmanrashid@Ashikurs-Mac-mini.local>
2022-11-06 15:39:21 +0600

    added doctor registartion design

--- src/main/java/com/example/javafx_hospital_management_system/HelloController.java
+++ src/main/java/com/example/javafx_hospital_management_system/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.javafx_hospital_management_system;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label label;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField txtName;
+
+    @FXML
+    private TextField txtMobile;
+
+    @FXML
+    private TextField txtDepartment;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> IDColmn;
+
+    @FXML
+    private TableColumn<?, ?> NameColmn;
+
+    @FXML
+    private TableColumn<?, ?> MobileColmn;
+
+    @FXML
+    private TableColumn<?, ?> DepartmentColmn;
+
 }
